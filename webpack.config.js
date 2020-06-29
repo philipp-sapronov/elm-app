0c40054f08595b7ad95e6a5347a7e06c332b33b4
philipp.sapronov <philipp.sapronov@gmail.com>
2020-06-29 12:53:10 +0300

    add public Path to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,19 @@ const ElmLoaders = (...loaders) =>
 
 module.exports = {
   mode: NODE_ENV,
+  devServer: {
+    contentBase: false,
+    historyApiFallback: true,
+    host: "0.0.0.0",
+    hot: true,
+    publicPath: "/",
+    liveReload: true,
+    port: 8080,
+  },
   entry: path.join(src, "index.js"),
   output: {
     path: path.resolve(__dirname, "./dist"),
+    publicPath: "/",
     filename: "[name].bundle.js",
   },
   devtool: isDevMode ? "source-map" : false,
