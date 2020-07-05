2f351239e37a990d4c41294bccee0cdc5adcdc8c
philipp.sapronov <philipp.sapronov@gmail.com>
2020-07-05 20:51:58 +0300

    fix hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,18 @@
-var path = require("path");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const NODE_ENV = process.env.NODE_ENV;
 const isDevMode = NODE_ENV === "development";
 const src = path.join(__dirname, "src");
 const elm = path.join(__dirname);
-const miniCss = require("mini-css-extract-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
   mode: NODE_ENV,
   devServer: {
     contentBase: false,
     historyApiFallback: true,
     host: "0.0.0.0",
-    hot: true,
     publicPath: "/",
-    liveReload: true,
     port: 3000,
   },
   entry: path.join(src, "index.js"),
@@ -28,6 +25,20 @@ module.exports = {
 
   module: {
     rules: [
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: [
+          {
+            loader: "elm-hot-webpack-loader",
+            options: { cwd: elm },
+          },
+          {
+            loader: "elm-webpack-loader",
+            options: { cwd: elm },
+          },
+        ],
+      },
       {
         test: /\.(sass|scss)$/,
         use: ExtractTextPlugin.extract({
@@ -48,20 +59,6 @@ module.exports = {
             },
           ],
         }),
-      },
-      {
-        test: /\.elm$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        use: [
-          {
-            loader: "elm-hot-webpack-loader",
-            options: { cwd: elm },
-          },
-          {
-            loader: "elm-webpack-loader",
-            options: { cwd: elm },
-          },
-        ],
       },
     ],
   },
