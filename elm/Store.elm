84712e124f2dace0eab3db12ff2842bde4bd12a0
philipp.sapronov <philipp.sapronov@gmail.com>
2020-07-05 14:16:32 +0300

    add Store module

---
+++ elm/Store.elm
@@ -0,0 +1,26 @@
+module Store exposing (..)
+
+
+type alias Counter =
+    { value : Int
+    }
+
+
+type alias Store =
+    { homeCounter : Counter
+    , blogCounter : Counter
+    , notFoundTitle : Heaidng
+    }
+
+
+type alias Heaidng =
+    { value : String
+    }
+
+
+store : Store
+store =
+    { homeCounter = { value = 0 }
+    , blogCounter = { value = 0 }
+    , notFoundTitle = { value = "NotFound" }
+    }
