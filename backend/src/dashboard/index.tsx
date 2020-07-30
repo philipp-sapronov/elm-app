2319f7ff207e27b078ffacc71e0cbc5e528a6a3f
philipp.sapronov <philipp.sapronov@gmail.com>
2020-07-31 00:44:50 +0300

    backend: use routes with slug

--- backend/src/dashboard/index.tsx
+++ backend/src/dashboard/index.tsx
@@ -10,12 +10,11 @@ import { Settings } from "./containers/settings";
 import { Menu } from "./containers/menu";
 
 export const dashboardRoutes = [
-  <Route key={"dashboard"} path={"/posts"} exact component={Posts} />,
-  <Route key={"dashboard"} path={"/posts/:slug"} exact component={Posts} />,
-  <Route key={"dashboard"} path={"/categories"} exact component={Categories} />,
+  <Route key={"dashboard"} path={"/posts/:slug?"} exact component={Posts} />,
+  <Route key={"dashboard"} path={"/categories/:slug?"} exact component={Categories} />,
   <Route key={"dashboard"} path={"/media"} exact component={Media} />,
   <Route key={"dashboard"} path={"/settings"} exact component={Settings} />,
-  <Route key={"dashboard"} path={"/tags"} exact component={Tags} />,
+  <Route key={"dashboard"} path={"/tags/:slug?"} exact component={Tags} />,
   <Route key={"dashboard"} path={"/users"} exact component={Users} />,
   <Route key={"dashboard"} path={"/menu"} exact component={Menu} />,
   <Route key={"dashboard-alias"} path={"/dashboard"} exact component={Dashboard} />,
